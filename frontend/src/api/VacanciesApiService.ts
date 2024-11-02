65e67c5ae601ad97eb9f764619f00ae72bc02718
ShmatEugene <shmat.evg@yandex.ru>
2024-11-02 21:55:00 +0300

    front: создание вакансии

--- frontend/src/api/VacanciesApiService.ts
+++ frontend/src/api/VacanciesApiService.ts
@@ -2,19 +2,6 @@ import { API_URL } from '@/config';
 import { CreateVacancyParams, FetchVacancyParams, Vacancy } from './models';
 import { get, post } from './http';
 
-// interface Vacancy {
-//     id: number;
-//     title: string;
-//     description: string;
-//     grade: Grade;
-//     competencies: Competency[];
-// }
-
-// interface Competency {
-//     name: string;
-//     proficiency: number;
-// }
-
 class VacanciesApiService {
     public async createVacancy(params: CreateVacancyParams) {
         const response = await post(`${API_URL}/api/v1/vacancies`, params);
@@ -23,21 +10,6 @@ class VacanciesApiService {
     }
 
     public async fetchVacancies(params: FetchVacancyParams) {
-        return [
-            {
-                id: 1,
-                title: 'title',
-                description: 'description',
-                grade: 'junior',
-                competencies: [
-                    {
-                        name: 'name',
-                        proficiency: 1,
-                    },
-                ],
-            },
-        ];
-
         const response = await get<Vacancy[]>(`${API_URL}/api/v1/vacancies`, { params });
 
         return response;
