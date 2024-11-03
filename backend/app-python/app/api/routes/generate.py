147cb02f31c56ff698361bbbaddc21f5dbaf9cba
lisaanthro <liza_vater@yahoo.com>
2024-11-03 22:05:41 +0300

    fix: candidate metrix calculation

--- backend/app-python/app/api/routes/generate.py
+++ backend/app-python/app/api/routes/generate.py
@@ -17,38 +17,27 @@ class Feedback(BaseSchema):
     message: str
 
 
-@router.get("/feedback/approve/vacancies/{vacancy-id}/candidates/{candidate_id}")
+@router.get("/feedback/approve/candidates/{candidate_id}")
 async def generate_approve_feedback(
         db_session: SessionDep,
         db_user: CurrentUser,
-        vacancy_id: int,
         candidate_id: int,
 ) -> Feedback:
-    application = crud.application.get_by_candidate_and_vaccancy(db_session, candidate_id, vacancy_id)
 
     status = "pending"
-    if application:
-        status = application.status
 
     candidate = serializers.candidate.get_candidate(crud.candidate.get_candidate(db_session, candidate_id))
-    vacancy = serializers.vacancy.get_vacancy(crud.vacancy.get_vacancy(db_session, vacancy_id))
 
     candidate_data = {
-        "name": candidate.nicknamename,
+        "nickname": candidate.nicknamename,
         "position": vacancy.position,
         "summary": candidate.summary,
-    }
-
-    vacancy_data = {
-        "position": vacancy.position,
-        "description": vacancy.description,
     }
 
     response = requests.post(
         f"{os.environ.get('ML_RESUME_HOST', 'http://localhost')}:5000/feedback/generate",
         json={
             "action": "approve",
-            "vacancy": vacancy_data,
             "candidate": candidate_data,
             "status": status,
         },
